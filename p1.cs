1389dee225286918e51b93e5fb3a36fbc1742d84
Rustam Ali Virk <virkrustamali@gmail.com>
2021-12-02 22:33:23 +0500

    first commit

---
+++ p1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Cir = new circle();
+            float g = Cir.GetRadius(4);
+            Cir.Area();
+            Cir.Circumference();
+        }
+    }
+}
---
+++ p2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new mark();
+            result.Subject_1 = 43;
+            result.Subject_2 = 40;
+            result.Subject_3 = 41;
+            result.Sum();
+            result.Average();
+        }
+    }
+}
